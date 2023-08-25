fa810c7c1d50a4f7e904e87daaf41650845df9cc
Luciano Scola <lucianoluigiscola@gmail.com>
2023-08-25 15:34:57 -0300

    Ejercicio 6

---
+++ Practica1/eje_6.hs
@@ -0,0 +1,41 @@
+import Parsing
+import Data.Char
+import Control.Monad
+import Control.Applicative hiding (many)
+
+type HetLists = ([Int],String)
+
+charSep :: Char -> Parser ()
+charSep c = do many (char c)
+               return ()
+
+heter :: Parser ([Int], String)
+heter = do heter1
+           <|>
+           return ([],[])
+
+heter1 :: Parser ([Int], String)
+heter1 = do char ','
+            x <- heter1
+            return x
+            <|>
+            do char '\''
+               c <- letter
+               char '\''
+               (ds,cs) <- heter
+               return (ds, (c:cs))
+               <|>
+               do d <- digit 
+                  (ds,cs) <- heter
+                  return (((digitToInt d):ds),cs)
+                  
+                     
+
+heterogeneus :: Parser ([Int], String)
+heterogeneus = do char '['
+                  l <-  heter 
+                  char ']'
+                  return l 
+
+              
+
