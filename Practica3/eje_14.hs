fd0f505c0d280ad64e13ac5e5afd70962cd97eb4
Luciano Scola <lucianoluigiscola@gmail.com>
2023-09-23 02:18:15 -0300

    Practica3

---
+++ Practica3/eje_14.hs
@@ -0,0 +1,68 @@
+-- Teste para el ejercicio 14
+--
+--
+---------------------------------
+
+data Nat = Zero | Succ Nat deriving Show
+
+cuatro = numNat 4
+cinco = numNat 5
+seis = numNat 6
+diez = numNat 10
+
+
+
+foldn :: Nat -> (a -> a) -> a -> a
+foldn Zero f z = z
+foldn (Succ n) f z = f (foldn n f z)
+
+numNat :: Int -> Nat
+numNat 0 = Zero
+numNat n = Succ (numNat (n - 1))
+
+isZero :: Nat -> Bool
+isZero n = foldn n (\n -> False) True 
+
+showNat :: Nat -> Int
+showNat n = foldn n (1+) 0 
+
+sumaNat :: Nat -> Nat -> Nat  
+sumaNat n m = foldn m Succ n
+
+predNat' :: Nat -> (Nat, Nat)
+predNat' n = foldn n (\(f,s) -> (Succ f, f)) (Zero,Zero) 
+
+predNat :: Nat -> Nat
+predNat n = s
+        where
+              (_,s) = predNat' n
+
+maxNat :: Nat -> Nat -> Nat
+maxNat m n = if isZero (subNat m n) then n else m
+
+subNat :: Nat -> Nat -> Nat
+subNat m n = foldn n predNat m
+
+data BinTree a = Leaf | Bin a (BinTree a) (BinTree a) deriving Show
+
+dataTree = (Bin 9
+           (Bin 4 (Bin 0 (Bin 0 Leaf Leaf) Leaf) (Bin 1 Leaf Leaf))
+           (Bin 6 (Bin 15 Leaf Leaf) (Bin 10 Leaf Leaf)))
+
+foldBin :: BinTree a -> b -> (a -> b -> b -> b) -> b
+foldBin Leaf l b = l
+foldBin (Bin a t u ) l b = b a (foldBin t l b) (foldBin u l b)
+
+isLeaf :: BinTree a -> Bool
+isLeaf d = foldBin d True (\a -> \l -> \r -> False)
+
+mapBin :: (a -> b) -> BinTree a -> BinTree b
+mapBin f d = foldBin d Leaf (\a -> \l -> \r -> (Bin (f a) l r))
+
+heightBin :: BinTree a -> Nat
+heightBin d = foldBin d Zero (\a -> \l -> \r -> (Succ (maxNat l r)))
+
+mirrorBin :: BinTree a -> BinTree a
+mirrorBin d = foldBin d Leaf (\a -> \l -> \r -> (Bin a r l))
+
+
