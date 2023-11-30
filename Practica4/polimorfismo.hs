080ae5862996cc651164c07a8b7213fdb0b26590
Luciano Scola <lucianoluigiscola@gmail.com>
2023-11-30 15:22:46 -0300

    format practice 4

--- Practica4/polimorfismo.hs
+++ Practica4/polimorfismo.hs
@@ -7,17 +7,17 @@ newtype Nat = Nat {runNat :: forall a. (a -> a) -> a -> a}
 
 data MyNat = Zero | Succ MyNat
 
+nat :: Int -> Nat
+nat 0 = zero
+nat n = 
+    let r = nat $ n - 1 
+    in Nat $ \f z -> f $ runNat r f z
+
 zero :: Nat
 zero = Nat $ \f z -> z
 
-one :: Nat
-one = Nat $ \f z -> f z
-
 suc :: Nat -> Nat
 suc n = Nat $ \f z -> f $ runNat n f z
-
-sumN :: Nat -> Nat -> Nat
-sumN n1 = runNat n1 suc
 
 double:: forall a. (a -> a) -> a -> a
 double f x = f $ f x
@@ -30,9 +30,6 @@ t = MyBool const
 f :: MyBool
 f = MyBool $ \x y -> y
 
-myAnd :: MyBool -> MyBool -> MyBool
-myAnd b1 b2 = MyBool $ \x y -> runBool b1 (runBool b2 x y) y
-
 newtype Pair b c = P {runP :: forall a. (b -> c -> a) -> a}
 
 pairP ::forall a b. a -> b -> Pair a b
@@ -44,23 +41,6 @@ fstP p = runP p const
 sndP ::forall a b. Pair a b -> b
 sndP p = runP p (\x y -> y)
 
-pairNat :: Nat -> Nat -> Pair Nat Nat
-pairNat n1 n2 = P $ \f -> f n1 n2
-
-firstNat :: Pair Nat Nat -> Nat
-firstNat p = runP p const
-
-secondNat :: Pair Nat Nat -> Nat
-secondNat p = runP p (\x y -> y)
-
-pred' :: Nat -> Pair Nat Nat
-pred' n = runNat n shiftAdd $ pairNat zero zero
-
-shiftAdd :: Pair Nat Nat -> Pair Nat Nat
-shiftAdd p = runP p (\n1 n2 -> pairNat (suc n1) n1)
-
-predNat :: Nat -> Nat
-predNat n = secondNat $ pred' n
 
 newtype List a = List {runList :: forall r. (a -> r -> r) -> r -> r}
 
@@ -73,6 +53,36 @@ cons a l = List $ \ c as -> c a $ runList l c as
 null :: forall a. List a -> MyBool
 null l = MyBool $ \t f -> runList l (\c n -> f) t
 
+-- Ejercicio 11
+
+myAnd :: MyBool -> MyBool -> MyBool
+myAnd b1 b2 = MyBool $ \x y -> runBool b1 (runBool b2 x y) y
+
+-- Ejercicio 12
+
+pairNat :: Nat -> Nat -> Pair Nat Nat
+pairNat n1 n2 = P $ \f -> f n1 n2
+
+firstNat :: Pair Nat Nat -> Nat
+firstNat p = runP p const
+
+secondNat :: Pair Nat Nat -> Nat
+secondNat p = runP p (\x y -> y)
+
+-- Ejercicio 13
+
+pred' :: Nat -> Pair Nat Nat
+pred' n = runNat n shiftAdd $ pairNat zero zero
+
+shiftAdd :: Pair Nat Nat -> Pair Nat Nat
+shiftAdd p = runP p (\n1 n2 -> pairNat (suc n1) n1)
+
+predNat :: Nat -> Nat
+predNat n = secondNat $ pred' n
+
+-- Ejercicio 14
+
+    -- a)
 mapL :: forall a b. (a -> b) -> List a -> List b
 mapL f l = List $ \c n -> runList l (c . f) n
 
@@ -82,30 +92,33 @@ appendL l1 = runList l1 cons
 reverseL :: forall a. List a -> List a
 reverseL l = runList l (\h tl -> appendL tl (cons h nil)) nil
 
+    -- b)
+
+sumN :: Nat -> Nat -> Nat
+sumN n1 = runNat n1 suc
+
+sumL :: List Nat -> Nat
+sumL l = runList l sumN zero
+
+    -- C)
+
 singleton :: forall a . a -> List a
 singleton a = cons a nil
 
 headTail :: forall a . List a -> Pair (List a) (List a)
 headTail l = runList l (\a r -> pairP (appendL (singleton a) (fstP r)) (sndP r)) (pairP nil nil)
 
-headL :: forall a. List a -> List a
-headL l = fstP $ headTail l -- fstP headTail
-
 tailL :: forall a. List a -> List a
 tailL l = sndP $ headTail l
 
-sumL :: List Nat -> Nat
-sumL l = runList l sumN zero
-
-
-headLL :: forall a. List a -> a -> a
-headLL l b = runList l (\a r -> a) b
+headL :: forall a. List a -> a -> a
+headL l = runList l const
 
 insert :: forall a. (a -> a -> MyBool) -> List a -> a -> List a
 insert cmp l a =
     runList l
     (\b r ->
-       runBool (cmp b (headLL r b))
+       runBool (cmp b (headL r b))
        (cons b r)
-       (cons (headLL r b) (cons b (tailL r))))
+       (cons (headL r b) (cons b (tailL r))))
     (singleton a)
